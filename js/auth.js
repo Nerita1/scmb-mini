434ae78f41b999e2dde923caecbb4b8103fc00df
Nerita1 <Neritamelda@gmail.com>
2026-01-30 22:32:15 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 // ================================
 // KONEKSI KE SUPABASE
 // ================================
-const sb = supabaseJs.createClient(
+const sb = supabase.createClient(
   "https://vckarhellnkghsnnfmgn.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZja2FyaGVsbG5rZ2hzbm5mbWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3ODA2NDAsImV4cCI6MjA4NTM1NjY0MH0.4D_Xb22o4n5JzR2twZzZUaV1yj5P0seTXMeZXNQyu1s"
 );
